90a88b9cfeaade5fe84f24a9a4a9fe07711c707b
Saquib Khan <ksaquib54@gmail.com>
2021-04-05 20:51:59 +0530

    answer modal done

--- src/App.js
+++ src/App.js
@@ -1,30 +1,71 @@
-import React from 'react';
-import Question from './components/Question';
-import CategorySelector from './components/CategorySelector';
-import ResultModal from './components/ResultModal';
-import Scoreboard from './components/Scoreboard';
-import './App.css';
+import React from "react";
+import { useEffect, useState, useCallback } from "react";
+import Question from "./components/Question";
+import CategorySelector from "./components/CategorySelector";
+import ResultModal from "./components/ResultModal";
+import Scoreboard from "./components/Scoreboard";
+import "./App.css";
 
 export default function App() {
+  const [question, setQuestion] = useState(null);
+  const [selectedCategory, setSelectedCategory] = useState("any");
+  const [isCorrect, setIsCorrect] = useState(null);
+  const [correctScore, setCorrectScore] = useState(0);
+  const [wrongScore, setWrongScore] = useState(0);
+  const getQuestion = useCallback(() => {
+    setIsCorrect(null);
+    let url = "https://opentdb.com/api.php?amount=1";
+    if (selectedCategory != "any") url += `&category=${selectedCategory}`;
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        setQuestion(data.results[0]);
+      });
+  }, []);
+
+  useEffect(() => {
+    getQuestion();
+  }, [getQuestion, selectedCategory]);
+
+  function handleQuestionAnswered(answer) {
+    const isAnswerCorrect = answer === question.correct_answer;
+    setIsCorrect(isAnswerCorrect);
+    if (isAnswerCorrect) setCorrectScore((score) => score + 1);
+    else setWrongScore((score) => score + 1);
+  }
   return (
     <div className="app">
       {/* show the result modal ----------------------- */}
-      {/* <ResultModal /> */}
+      {isCorrect != null && (
+        <ResultModal
+          isCorrect={isCorrect}
+          question={question}
+          getQuestion={getQuestion}
+        />
+      )}
 
       {/* question header ----------------------- */}
       <div className="question-header">
-        <CategorySelector />
-        <Scoreboard />
+        <CategorySelector
+          category={selectedCategory}
+          chooseCategory={setSelectedCategory}
+        />
+        <Scoreboard correct={correctScore} wrong={wrongScore} />
       </div>
 
       {/* the question itself ----------------------- */}
       <div className="question-main">
-        <Question />
+        {question && (
+          <Question
+            question={question}
+            answerQuestion={handleQuestionAnswered}
+          />
+        )}
       </div>
 
       {/* question footer ----------------------- */}
       <div className="question-footer">
-        <button>Go to next question 👉</button>
+        <button onClick={getQuestion}>Go to next question 👉</button>
       </div>
     </div>
   );
--- src/components/Question.js
+++ src/components/Question.js
@@ -1,14 +1,22 @@
-import React from 'react';
+import React from "react";
+import shuffle from "lodash.shuffle";
 
-const sampleAnswers = ['One', 'Two', 'Three', 'Four'];
+export default function Question({ question, answerQuestion }) {
+  const answers = shuffle([
+    ...question.incorrect_answers,
+    question.correct_answer,
+  ]);
 
-export default function Question() {
   return (
     <div className="question">
-      <h2>Question Here</h2>
+      <h2 dangerouslySetInnerHTML={{ __html: question.question }} />
 
-      {sampleAnswers.map((answer, index) => (
-        <button key={index}>answer</button>
+      {answers.map((answer, index) => (
+        <button
+          key={index}
+          onClick={() => answerQuestion(answer)}
+          dangerouslySetInnerHTML={{ __html: answer }}
+        />
       ))}
     </div>
   );
